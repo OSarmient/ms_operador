6bc4c5a8003b68b4edf41a0fdc0f6e1970c81d8b
yeison <ycuervob@unal.edu.co>
2023-04-14 00:57:49 -0500

    Pequeña correcion para evitar error en caso de faltar SECRET_KEY

--- utilities/jwtSings.js
+++ utilities/jwtSings.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const secretKey = process.env.SECRET_KEY;
+const secretKey = process.env.SECRET_KEY || 'secretKey';
 
 module.exports = (id_operador_asignado) => {
     return jwt.sign({
