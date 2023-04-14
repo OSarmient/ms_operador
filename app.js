b9dc7f2b12dbb84137aababb6b52628e4a0f2296
Omar Sarmiento <omarcamilo20@gmail.com>
2023-04-14 11:44:56 -0500

    comprovación de contraseña

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const indexRouter = require('./routes/index.js');
 const logIn = require('./routes/logIn.js');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
