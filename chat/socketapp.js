827edc14c80f6e5322191ec1d0f204a72bae74b7
ycuervob <64037012+ycuervob@users.noreply.github.com>
2023-04-18 15:15:41 -0500

    se actualiza socket app

--- chat/socketapp.js
+++ chat/socketapp.js
@@ -11,6 +11,15 @@ const io = require("socket.io")(httpServer, {
 
 io.on("connection", (socket) => {
 
+    socket.on("join", ({auth_token, id_operador_asignado})=>{
+        try{
+            const decoded = jwtValidate(auth_token);
+            socket.join(decoded.id_operador_asignado);
+        }catch(error){
+            socket.emit("error", "No autorizado");
+        }
+    });
+
     socket.on("message", ({auth_token, id_chat, mensaje})=>{
         try{
             const decoded = jwtValidate(auth_token);
