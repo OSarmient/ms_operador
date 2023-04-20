3d26791f1e5c5c33420490a67dbcaf8aa00856ea
yeison <ycuervob@unal.edu.co>
2023-04-20 12:15:49 -0500

    corrección de logIn doble send

--- routes/logIn.js
+++ routes/logIn.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const graphqlFetch = require('../utilities/fetchUtilitie.js');
-const {getOperadoresQuery} = require('../queries/getQueries.js');
+const { getOperadoresQuery } = require('../queries/getQueries.js');
 const matchPassword = require('../utilities/matchPassword.js');
 
 router.post('/', async (req, res, next) => {
-    
+
     try {
         const { username, password } = req.body;
         const theResponse = await graphqlFetch(getOperadoresQuery({ limit: 10, userid: username }));
 
         console.log(theResponse.data);
-        
-        if(theResponse.errors) {
+
+        if (theResponse.errors) {
             res.status(401).json({ message: 'Contraseña incorrecta.' });
-        }else{
+        } else {
             const isPasswordValid = await matchPassword(password, theResponse.data.operador[0].crp_contrasena, username);
-            if(!isPasswordValid) res.status(401).json({ message: 'Contraseña incorrecta.' });
-            res.status(200).json({ auth_token: isPasswordValid});
+            if (!isPasswordValid) { res.status(401).json({ message: 'Contraseña incorrecta.' }); return; };
+            res.status(200).json({ auth_token: isPasswordValid });
         }
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Error interno del servidor.' });
     }
-    
+
 });
 
 
