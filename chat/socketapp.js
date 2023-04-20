2306fe9febeb560620b7f354782b8b22a7278939
yeison <ycuervob@unal.edu.co>
2023-04-19 20:12:56 -0500

    Se añade flujo de autorizacion del socket app

--- chat/socketapp.js
+++ chat/socketapp.js
@@ -9,25 +9,23 @@ const io = require("socket.io")(httpServer, {
     }
 });
 
+io.use((socket, next) => {
+    console.log(socket.request.headers);
+    try {
+        const decoded = jwtValidate(socket.request.headers.auth_token);
+        socket.join(decoded.id_operador_asignado);
+        socket.decoded = decoded;
+        next();
+    } catch (error) {
+        socket.emit("error", "No autorizado");
+        next(new Error("invalid"));
+    }
+});
+
 io.on("connection", (socket) => {
 
-    socket.on("join", ({ auth_token }) => {
-        try {
-            const decoded = jwtValidate(auth_token);
-            socket.join(decoded.id_operador_asignado);
-        } catch (error) {
-            socket.emit("error", "No autorizado");
-        }
-    });
-
-    socket.on("message", ({ auth_token, id_chat, mensaje }) => {
-        try {
-            const decoded = jwtValidate(auth_token);
-            socket.join(decoded.id_operador_asignado);
-            recibirMensajeResolver({ id_chat, mensaje });
-        } catch (error) {
-            socket.emit("error", "No autorizado");
-        }
+    socket.on("message", ({ id_chat, mensaje }) => {
+        recibirMensajeResolver({ id_chat, mensaje });
     });
 
 });
