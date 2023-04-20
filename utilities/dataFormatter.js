402da441242a50f4feffe7045a32b7b82b0fb1fa
yeison <ycuervob@unal.edu.co>
2023-04-20 00:29:05 -0500

    Se corrigio el mes que salia uno antes

--- utilities/dataFormatter.js
+++ utilities/dataFormatter.js
@@ -1,11 +1,11 @@
 module.exports = (d) => {
-    var [dia, mes, anio, hora, minutos, segundos] = [d.getDate(), d.getMonth(), d.getFullYear(), d.getHours(), d.getMinutes(), d.getSeconds()];
+    var [dia, mes, anio, hora, minutos, segundos] = [d.getDate(), d.getMonth()+1, d.getFullYear(), d.getHours(), d.getMinutes(), d.getSeconds()];
     var dia = dia < 10 ? "0" + dia : dia;
     var mes = mes < 10 ? "0" + mes : mes;
     var hora = hora < 10 ? "0" + hora : hora;
     var minutos = minutos < 10 ? "0" + minutos : minutos;
     var segundos = segundos < 10 ? "0" + segundos : segundos;
-    var text = `${dia}/${mes}/${anio} ${hora}:${minutos}:${segundos}`;
+    var text = `${anio}/${mes}/${dia} ${hora}:${minutos}:${segundos}`;
     
     return text;
 }
